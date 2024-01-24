98188bfe26ecefbff86ba0da0e2e75f5935ffd27
vjannu55 <146692732+vjannu55@users.noreply.github.com>
2024-01-24 20:44:47 +0500

    Styling Files added

--- app/src/main/java/com/vijayjannu/assignment/MainActivity.kt
+++ app/src/main/java/com/vijayjannu/assignment/MainActivity.kt
@@ -2,9 +2,15 @@ package com.malikosft.assignment
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.preferencesKey
 
 
 class MainActivity : ComponentActivity() {
+
+    lateinit var dataStore: DataStore<Preferences>
+    val key  = preferencesKey<String>("username")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/vijayjannu/assignment/ui/theme/Color.kt
@@ -0,0 +1,2 @@
+package com.vijayjannu.assignment.ui.theme
+
