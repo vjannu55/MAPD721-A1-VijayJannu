ff6b004a92c459e1eac28886d6ad44812f4875c8
vjannu55 <146692732+vjannu55@users.noreply.github.com>
2024-01-24 23:15:50 +0500

    Empty Screen
    Creation of Empty Screen with Initialization of Essential Variables for Name, Email and Id in Form Function

--- app/src/main/java/com/vijayjannu/assignment/MainActivity.kt
+++ app/src/main/java/com/vijayjannu/assignment/MainActivity.kt
@@ -1,23 +1,54 @@
 package com.malikosft.assignment
+import android.annotation.SuppressLint
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.datastore.core.DataStore
+import androidx.datastore.createDataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.preferencesKey
+import androidx.datastore.preferences.createDataStore
+import com.malikosft.assignment.ui.theme.AssignmentTheme
 
 
 class MainActivity : ComponentActivity() {
-
-    lateinit var dataStore: DataStore<Preferences>
-    val key  = preferencesKey<String>("username")
-
+    lateinit var dataStore: DataStore<androidx.datastore.preferences.core.Preferences>
+    val key = preferencesKey<String>("username")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        dataStore = createDataStore("setting")
         setContent {
+            AssignmentTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    form_filling()
+
+                }
+            }
         }
     }
+}
+@Preview
+@SuppressLint("SuspiciousIndentation")
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun form_filling() {
 
+    val name = remember { mutableStateOf("") }
+    val email = remember { mutableStateOf("") }
+    val id = remember { mutableStateOf("452") }
 
 }
-
